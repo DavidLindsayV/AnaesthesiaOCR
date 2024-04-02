61948d6a0db1579bea1b3b2a3c97cc60a2aa7cf1
David Lindsay <davidtlindsay@gmail.com>
2024-04-02 23:10:15 +1300

    Added more AIs to process fields from images

--- process_img.py
+++ process_img.py
@@ -5,6 +5,8 @@ def resize_height(image, ideal_height):
     return image.resize((int(image.width/image.height * ideal_height), ideal_height))
 
 def get_parameter_imgs(modified_image):
+    #parameters = 
+    # rtime	ecg.hr	ecg.st1	ecg.st2	ecg.st3	ecg.imp_rr	p1.sys	p1.dia	p1.mean	p1.hr	p2.sys	p2.dia	p2.mean	p2.hr	p3.sys	p3.dia	p3.mean	p3.hr	p4.sys	p4.dia	p4.mean	p4.hr	nibp.sys	nibp.dia	nibp.mean	nibp.hr	t1.temp	t2.temp	t3.temp	t4.temp	spo2.SpO2	spo2.pr	spo2.ir_amp	co2.et	co2.fi	co2.rr	co2.amb_press	o2.et	o2.fi	n2o.et	n2o.fi	aa.et	aa.fi	aa.mac_sum	p5.sys	p5.dia	p5.mean	p5.hr	p6.sys	p6.dia	p6.mean	p6.hr
 
     #Get a subsection that is the heart rate
     heartrate_image = modified_image.crop((500, 160, 580, 230))  #TODO ask Michael how many digits each of these can be, and whether the number of digits changes their positions
--- query.py
+++ query.py
@@ -1,49 +1,58 @@
 ###Where querying the AI occurs###
 
-from io import BytesIO
-import json
-import numpy
-import requests
 import os
-import base64
-from PIL import Image
-import cv2
+from nanonets import NANONETSOCR
+import json
+
+import pytesseract
+import requests
 
 from process_img import process_img
 
-# model_id = "dd80ac37-ed9e-405c-87c9-a3285785677a" #This model is a 0-training model
 
 
-#Pixel coordinates of each Physiological parameter:
-#Heart rate x:500-580 y:160-230
-
-
-api_key = os.getenv("NANONETS_API_KEY")
-
-url = "https://app.nanonets.com/api/v2/OCR/FullText"
+model = "Nanonets_Requests"
 
 imagesDict = process_img("image.png")
 
+filenames = {}
 files = []
 for key in imagesDict.keys():
-    files.append( ('file', (key, open(os.path.join("processed_images", key + "-img.png"), 'rb'), 'application/pdf')) )
-
-# files=[
-#  ('file',('ecto2-img',open('processed_images/ecto2-img.png','rb'),'application/pdf')), 
-#   ('file',('heartrate-img',open('processed_images/heartrate-img.png','rb'),'application/pdf'))
-# ]
-
-headers = {}
+    filenames[key] = os.path.join("processed_images", key + "-img.png")
+    files.append( ('file', (key, open(filenames[key]), 'application/pdf')))
+    # files.append( ('file', (key, open(os.path.join("processed_images", key + "-img.png"), 'rb'), 'application/pdf')) )
 
 
-response = requests.request("POST", url, headers=headers, files=files, auth=requests.auth.HTTPBasicAuth(api_key, ''))
-response = response.json()
-# print(response)
+if model == "Nanonets_pythonOCR":
+    model = NANONETSOCR()
+    api_key = os.getenv("NANONETS_API_KEY")
+    model.set_token(api_key)
 
-for result in response['results']:
-    print(result['filename'] + " " + result['page_data'][0]['words'][0]['text'].strip())
+    for key, value in filenames.items():
+        pred_json = model.convert_to_prediction(value)
+        print(key + " " + pred_json['results'][0]['page_data'][0]['words'][0]['text'].strip())        
 
+elif model == "Nanonets_Requests":
+    reqFiles = []
+    for key in imagesDict.keys():
+        reqFiles.append( ('file', (key, open(os.path.join("processed_images", key + "-img.png"), 'rb'), 'application/pdf')) )
 
-# print(response['results'][0]['page_data'][0]['raw_text'].strip()) #this expression extracts the raw text from an entire image, all texts together
-# print(response['results'][0]['page_data'][0]['words'][0]['text'].strip()) #this expression extracts the first bit of text found. However, unreliable if no text is found
+    headers = {}
+    api_key = os.getenv("NANONETS_API_KEY")
+    url = "https://app.nanonets.com/api/v2/OCR/FullText"
 
+    response = requests.request("POST", url, headers=headers, files=reqFiles, auth=requests.auth.HTTPBasicAuth(api_key, ''))
+    response = response.json()
+
+    for result in response['results']:
+        print(result['filename'] + " " + result['page_data'][0]['words'][0]['text'].strip())
+
+    # print(response['results'][0]['page_data'][0]['raw_text'].strip()) #this expression extracts the raw text from an entire image, all texts together
+    # print(response['results'][0]['page_data'][0]['words'][0]['text'].strip()) #this expression extracts the first bit of text found. However, unreliable if no text is found
+
+elif model == "PyTesseract":
+
+    print(filenames)
+    for key, value in filenames.items():
+        text = pytesseract.image_to_string(value,lang='eng')
+        print(key + " " + text)
