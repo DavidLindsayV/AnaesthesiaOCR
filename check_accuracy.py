c74d4180121d435e13920465bc44b470a3b3e438
David Lindsay <davidtlindsay@gmail.com>
2024-05-15 00:15:44 +1200

    Added calcuation of edit distance and CER

--- check_accuracy.py
+++ check_accuracy.py
@@ -1,12 +1,17 @@
+import re
+import sys
 import pandas as pd
-
+from nltk.metrics.distance import edit_distance
 import csv
+from jiwer import cer
+
+
 
 fields = ['ecg.hr', 'co2.et', 'co2.fi', 'co2.rr', 'p1.sys', 'p1.dia', 'p1.mean', 'aa.et', 'aa.fi']
    
 
 extracted_data = []
-with open("output.csv", 'r') as file:
+with open("easyocr_86.5.csv", 'r') as file:
     csvreader = csv.reader(file)
     firstrow = True
     columns = []
@@ -49,26 +54,44 @@ for row in range(0, dataframe1.max_row):
 
 print("Expected data loaded")
 
+if(len(expected_data) != len(extracted_data)):
+    print("ERROR: Length of expected data doesn't match length of extracted data. Ending program")
+    sys.exit()
+
 print("Number of images in OCR output: " + str(len(extracted_data)) + " \nNumber of images in testing data: " + str(len(expected_data)))
 num_correct = 0
-num_incorrect = 0
-field_correct = [0 for x in range(len(fields))]
+tot_edit_dist = 0
+tot_cer = 0
+tot_numerical_dist = 0
+eval_params = [[0 for y in range(3)] for x in range(len(fields))]   #this is a 2d array. The first index is what the field is. The second array is of [count_of_correct_ocr, total_edit_distance, total_cer]
+
 for i in range(len(extracted_data)):
-    print(expected_data[i])
-    print(extracted_data[i])
+    # print(expected_data[i])
+    # print(extracted_data[i])
     for j in range(len(fields)):
         exp = expected_data[i][j]
         extr = extracted_data[i][j]
         if exp == extr:
             num_correct += 1
-            field_correct[j] += 1
-        else:
-            num_incorrect += 1
-    acc_so_far = num_correct/(num_correct + num_incorrect)
-    # print("ACCURACY SO FAR: " + str(num_correct/(num_correct + num_incorrect)))
+            eval_params[j][0] += 1
+        tot_edit_dist += edit_distance(exp, extr)
+        if(edit_distance(exp, extr) > 5):
+            print(exp + " " + extr)
+        tot_cer += cer(exp, extr)
+        eval_params[j][1] += edit_distance(exp, extr)
+        eval_params[j][2] += cer(exp, extr)
+        # if re.search(r'[-+]?\d*\.?\d+', extr) and re.search(r'[-+]?\d*\.?\d+', exp):
+        #     exp = re.sub(r'[^0-9.]', '', exp)
+        #     extr = re.sub(r'[^0-9.]', '', extr)
+        #     tot_numerical_dist += abs(float(exp) - float(extr))
+
     print("Processed " + str(i+1) + " out of " + str(len(expected_data)))
 
-print("ACCURACY: " + "{:.1f}".format((num_correct/(num_correct + num_incorrect))*100) + "%") 
+tot_param_count = len(extracted_data) * len(fields)
+print("ACCURACY: " + "{:.1f}".format((num_correct/tot_param_count)*100) + "%") 
+print("AVG EDIT DISTANCE: " + "{:.3f}".format((tot_edit_dist/tot_param_count)))
+print("AVG CER: " + "{:.3f}".format((tot_cer/tot_param_count)))
+# print("AVG NUMERICAL DISTANCE: " + "{:.3f}".format((tot_numerical_dist/tot_param_count)))
 
 for i in range(len(fields)):
-    print("Accuracy for field " + str(fields[i]) + " = " + "{:.1f}".format((field_correct[i]/len(expected_data))*100) + "%") 
+    print("Field " + str(fields[i]) + " Accuracy = " + "{:.1f}".format((eval_params[i][0]/len(expected_data))*100) + "% Avg Edit Distance: " + "{:.3f}".format((eval_params[i][1]/len(expected_data))) + " Avg CER: " + "{:.3f}".format((eval_params[i][2]/len(expected_data)))) 
