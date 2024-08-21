eecc499553eadd0a5ba0c44ccbcfc5c453f05443
David Lindsay <davidtlindsay@gmail.com>
2024-08-21 15:45:21 +1200

    Updated write_to_csv to create 2 csv outputs, one with accurate text outputs (eg the text that was actually on the monitor screen) and the other with the EDDI formatted CSV

--- write_to_csv.py
+++ write_to_csv.py
@@ -19,7 +19,7 @@ def parse_number(string):
 
 
 def write_to_csv(ocr_data):
-    # field_names = ["rtime",	"ecg.hr",	"ecg.st1",	"ecg.st2",	"ecg.st3",	"ecg.imp_rr",	"p1.sys",	"p1.dia",	"p1.mean",	"p1.hr",	"p2.sys",	"p2.dia",	"p2.mean",	"p2.hr",	"p3.sys",	"p3.dia",	"p3.mean", 	"p3.hr",	"p4.sys",	"p4.dia",	"p4.mean",	"p4.hr",	"nibp.sys",	"nibp.dia",	"nibp.mean",	"nibp.hr",	"t1.temp",	"t2.temp",	"t3.temp",	"t4.temp",	"spo2.SpO2"	,"spo2.pr",	"spo2.ir_amp",	"co2.et",	"co2.fi",	"co2.rr",	"co2.amb_press",	"o2.et",	"o2.fi",	"n2o.et",	"n2o.fi",	"aa.et",	"aa.fi",	"aa.mac_sum",	"p5.sys",	"p5.dia",	"p5.mean",	"p5.hr",	"p6.sys",	"p6.dia",	"p6.mean",	"p6.hr"]
+    field_names = ["rtime",	"ecg.hr",	"ecg.st1",	"ecg.st2",	"ecg.st3",	"ecg.imp_rr",	"p1.sys",	"p1.dia",	"p1.mean",	"p1.hr",	"p2.sys",	"p2.dia",	"p2.mean",	"p2.hr",	"p3.sys",	"p3.dia",	"p3.mean", 	"p3.hr",	"p4.sys",	"p4.dia",	"p4.mean",	"p4.hr",	"nibp.sys",	"nibp.dia",	"nibp.mean",	"nibp.hr",	"t1.temp",	"t2.temp",	"t3.temp",	"t4.temp",	"spo2.SpO2"	,"spo2.pr",	"spo2.ir_amp",	"co2.et",	"co2.fi",	"co2.rr",	"co2.amb_press",	"o2.et",	"o2.fi",	"n2o.et",	"n2o.fi",	"aa.et",	"aa.fi",	"aa.mac_sum",	"p5.sys",	"p5.dia",	"p5.mean",	"p5.hr",	"p6.sys",	"p6.dia",	"p6.mean",	"p6.hr"]
     fields_to_multiply_by_100 = [
         "p1.mean",
         "p1.sys",
@@ -45,10 +45,18 @@ def write_to_csv(ocr_data):
         "t4.temp",
     ]
 
-    print(ocr_data)
+    data_writtenTextFormat = [] #the CSV which has text accurate to what was displayed on the screen. This matches with the excel file
+    for data_row in ocr_data:
+        dataDict = {}
+        for field in field_names:
+            if field in data_row:
+                dataDict[field] = data_row[field]
+            else:
+                dataDict[field] = ''
+        data_writtenTextFormat.append(dataDict)
 
     time = datetime.now()
-    data = []
+    data_EddiFormat = [] #the CSV which has text in the format EDDI expects
     for data_row in ocr_data:
         dataDict = {}
         dataDict["rtime"] = time.strftime("%d/%m/%Y %H:%M:%S")
@@ -62,22 +70,29 @@ def write_to_csv(ocr_data):
                     dataDict[field] = number
             else:
                 dataDict[field] = '' #the non numeric '' will be quoted and turn into ""
-        data.append(dataDict)
+        data_EddiFormat.append(dataDict)
 
     # File path to save the CSV file
-    csv_file_path = "output.csv"
+    csv_file_path_text = "output_text.csv"
+    csv_file_path_EDDI = "output_eddi.csv"
 
     # Writing data to CSV file
-    with open(csv_file_path, "w", newline="") as csv_file:
+    with open(csv_file_path_text, "w", newline="") as csv_file:
         # Define CSV writer object
-        writer = csv.DictWriter(csv_file, fieldnames=data[0].keys(), quoting=csv.QUOTE_NONNUMERIC)
+        writer = csv.DictWriter(csv_file, fieldnames=data_writtenTextFormat[0].keys(), quoting=csv.QUOTE_NONNUMERIC)
 
         # Write header
         writer.writeheader()
 
         # Write data
-        writer.writerows(data)
+        writer.writerows(data_writtenTextFormat)
+    
+    with open(csv_file_path_EDDI, "w", newline="") as csv_file:
+        # Define CSV writer object
+        writer = csv.DictWriter(csv_file, fieldnames=data_EddiFormat[0].keys(), quoting=csv.QUOTE_NONNUMERIC)
 
+        # Write header
+        writer.writeheader()
 
-# TODO:
-# - check new csv works with accuracy checking
+        # Write data
+        writer.writerows(data_EddiFormat)
