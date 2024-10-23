79a0b6237089ebf97827d64de66c1bda5b4e2083
David Lindsay <davidtlindsay@gmail.com>
2024-10-23 21:49:54 +1300

    Changed font sizes in make_rpi_speed_plot

--- make_rpi_speed_plot.py
+++ make_rpi_speed_plot.py
@@ -8,12 +8,12 @@ def make_graph_of_speeds(title, values, xlabel, ylabel):
     # Create a line plot
     plt.plot(x, values, marker='o', linestyle='', color='b')
     # Add title and labels
-    plt.title(title)
-    plt.ylabel(ylabel)
-    plt.xlabel(xlabel)
+    plt.title(title, fontsize=16)
+    plt.ylabel(ylabel, fontsize=14)
+    plt.xlabel(xlabel, fontsize=14)
     plt.xticks(range(0, len(values) + 1, 5))
     
     # Show the plot
     plt.show()
     
-make_graph_of_speeds("RPI Image Capture and SSH Speed", [1.2, 0.81, 0.82, 0.85, 0.86, 0.82, 0.88, 0.85, 0.87, 0.81, 0.85, 0.83], "Image number", "Time taken for image capture and ssh (seconds)")
+make_graph_of_speeds("RPI Image Capture and SSH Speed", [1.2, 0.81, 0.82, 0.85, 0.86, 0.82, 0.88, 0.85, 0.87, 0.81, 0.85, 0.83], "Image number", "Time taken for image capture and ssh (s)")
